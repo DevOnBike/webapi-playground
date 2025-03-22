542815b83325b2ef9475154c870dfd1972da87c6
DevOnBike <rychter.maciej@gmail.com>
2025-03-22 21:45:03 +0100

    akka.net publish to cluster

---
+++ Messaging/Configuration/AkkaOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging.Configuration
+{
+    public class AkkaOptions
+    {
+        public bool Enabled { get; set; }
+        public bool CmdEnabled { get; set; }
+    }
+}
